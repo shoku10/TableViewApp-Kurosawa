972fbf4d9f726b2556a948b8596704f2ae0dc2d0
shoku10 <shoku8544@gmail.com>
2023-08-29 18:10:22 +0900

    TextFieldが未入力の状態の時はタスクを追加しない仕様に変更

--- TableViewApp/AddListViewController.swift
+++ TableViewApp/AddListViewController.swift
@@ -25,12 +25,14 @@ class AddListViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // TextFieldが未入力の状態の時はタスクを追加しないようにする。
     @IBAction func addTask(_ sender: Any) {
-        let userDefaults = UserDefaults.standard
-        taskArray.append(addTextField.text!)
-        userDefaults.set(taskArray, forKey: "add")
-        self.navigationController?.popViewController(animated: true)
-        
+        if let newTask = addTextField.text, !newTask.isEmpty {
+            let userDefaults = UserDefaults.standard
+            taskArray.append(newTask)
+            userDefaults.set(taskArray, forKey: "add")
+            self.navigationController?.popViewController(animated: true)
+        } else {
+        }
     }
-    
 }
